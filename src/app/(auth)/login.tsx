fdd79caa657123c8a13a918ac68c7a2414b63078
hau <dauphaihau@gmail.com>
2025-01-03 11:27:28 +0700

    setup styling, src dir

---
+++ src/app/(auth)/login.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Login() {
+  return (
+    <View className="h-full justify-center items-center">
+      <Text className="text-5xl">Login page</Text>
+    </View>
+  );
+};
