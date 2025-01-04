0abb5ae839f3c32924ea72e9c5910fa27c6de04a
hau <dauphaihau@gmail.com>
2025-01-04 16:29:54 +0700

    chore: add eslint, tanstack query, configure supabase, custom some ui component,

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,40 +1,73 @@
 import '../../global.css';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme, DefaultTheme, Theme, ThemeProvider
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import React from 'react';
 import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { NAV_THEME } from '@/constants/theme';
+import { Platform } from 'react-native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
+
+const useIsomorphicLayoutEffect =
+  Platform.OS === 'web' && typeof window === 'undefined' ? React.useEffect : React.useLayoutEffect;
+
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const hasMounted = React.useRef(false);
+  const { isDarkColorScheme } = useColorScheme();
+  const queryClient = new QueryClient();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+
   const [loaded] = useFonts({
     SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+  useIsomorphicLayoutEffect(() => {
+    if (hasMounted.current) {
+      return;
     }
-  }, [loaded]);
+    setIsColorSchemeLoaded(true);
+    hasMounted.current = true;
+  }, []);
 
-  if (!loaded) {
+  // useEffect(() => {
+  //   if (loaded) {
+  //     SplashScreen.hideAsync();
+  //   }
+  // }, [loaded]);
+
+  if (!loaded || !isColorSchemeLoaded) {
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }}/>
-        <Stack.Screen name="(auth)/login" options={{ headerShown: false }}/>
-        <Stack.Screen name="+not-found"/>
-      </Stack>
-      <StatusBar style="auto"/>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+        <StatusBar style={isDarkColorScheme ? 'light' : 'dark'}/>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }}/>
+          <Stack.Screen name="(app)/home" options={{ title: 'Home' }}/>
+          <Stack.Screen name="(auth)/login" options={{ headerShown: false }}/>
+          <Stack.Screen name="(auth)/register" options={{ headerShown: false }}/>
+          <Stack.Screen name="+not-found"/>
+        </Stack>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
