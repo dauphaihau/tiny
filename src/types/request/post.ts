e4fb85621ef4e889bb98fbf8469678cb8efe8f51
hau <dauphaihau@gmail.com>
2025-02-04 11:36:26 +0700

    feat: filter posts current_profile follow

--- src/types/request/post.ts
+++ src/types/request/post.ts
@@ -2,16 +2,17 @@ import { Profile } from '@/types/models/profile';
 import { getDetailPost } from '@/services/post.service';
 import { Post } from '@/types/models/post';
 import { Database } from '@/types/database.types';
+import { GetListParams } from '@/types/request/common';
 
-export interface GetPostsParams {
-  limit: number;
-  page: number;
+export interface GetPostsParams extends GetListParams {
   current_profile_id: Profile['id']
+  type: Database['public']['Functions']['get_posts']['Args']['type'];
 }
 
-export interface GetPostsByProfileParams extends GetPostsParams {
+export interface GetPostsByProfileParams extends GetListParams {
   pr_id: Post['profile_id']
-  type?: 'posts' | 'replies'
+  type: Database['public']['Functions']['get_posts_by_profile']['Args']['type'];
+  current_profile_id: Profile['id']
 }
 
 export type PostResponse = Database['public']['Functions']['get_posts']['Returns'][0];
